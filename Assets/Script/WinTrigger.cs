6f4211c0ef25dd92af5003e23299967357092197
Namury <namuryah@gmail.com>
2021-06-06 18:08:12 +0700

    add finish state and credit

---
+++ Assets/Script/WinTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour
+{
+    public float restartDelay = 1f;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Win"); 
+        Invoke("Win", restartDelay);
+    }
+
+    // Update is called once per frame
+    void Win()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+}
+
